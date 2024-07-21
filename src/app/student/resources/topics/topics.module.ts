2769c231e85e0f9a9e56643e21d3b4e051e59875
Aditya Raj Aryan <adityaraja338@gmail.com>
2024-07-21 20:13:32 +0530

    student complete

--- src/app/student/resources/topics/topics.module.ts
+++ src/app/student/resources/topics/topics.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+// import { CommonModule } from '@angular/common';
 import { MaterialsComponent } from './materials/materials.component';
 import { RouterModule, Routes } from '@angular/router';
 import { TopicsComponent } from './topics.component';

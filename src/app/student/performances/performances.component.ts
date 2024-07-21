3f0bfcd5c3d180fd2f87fe071d3155b88c543024
Aditya Raj Aryan <adityaraja338@gmail.com>
2024-07-21 13:52:52 +0530

    Github init

---
+++ src/app/student/performances/performances.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-performances',
+  templateUrl: './performances.component.html',
+  styleUrl: './performances.component.scss'
+})
+export class PerformancesComponent {
+
+}

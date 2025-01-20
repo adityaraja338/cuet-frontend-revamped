352c350414181091f8f21aeda18808f10f4df529
Aditya Raj Aryan <adityaraja338@gmail.com>
2025-01-20 06:25:57 +0530

    Dashboard UI updated

--- src/app/student/dashboard/dashboard.component.ts
+++ src/app/student/dashboard/dashboard.component.ts
@@ -12,9 +12,6 @@ import { BaseChartDirective } from 'ng2-charts';
 export class DashboardComponent implements OnInit {
   previous = [];
 
-  popoverText = ''; // Text to show inside the popover
-  popoverVisible = false;
-
   subjects: any = [];
 
   events: any = [];
@@ -42,10 +39,10 @@ export class DashboardComponent implements OnInit {
   };
 
   averageScoreChartData: ChartConfiguration<'line'>['data'] = {
-    labels: ['-', '-', '-', '-', '-', '-', '-'],
+    labels: ['-', '-', '-', '-', '-', '-'],
     datasets: [
       {
-        data: [0, 0, 0, 0, 0, 0, 0],
+        data: [0, 0, 0, 0, 0, 0],
         label: 'Average',
         fill: true,
         tension: 0.4,
@@ -91,37 +88,6 @@ export class DashboardComponent implements OnInit {
       },
     },
   };
-
-  public barChartData: ChartConfiguration<'bar'>['data'] = {
-    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
-    datasets: [
-      {
-        data: [65, 59, 80, 81, 56, 55, 40],
-        label: 'Series A',
-        backgroundColor: '#8555FDD6',
-        borderRadius: 10,
-        maxBarThickness: 16,
-        // fill: true,
-      },
-    ],
-  };
-
-  public barChartOptions: ChartConfiguration<'bar'>['options'] = {
-    responsive: true,
-    scales: {
-      x: {
-        grid: {
-          display: false,
-        },
-      },
-      y: {
-        grid: {
-          display: false,
-        },
-      },
-    },
-  };
-  public barChartLegend = false;
 
   isPendingTest = false;
   pendingTestDetails: any;

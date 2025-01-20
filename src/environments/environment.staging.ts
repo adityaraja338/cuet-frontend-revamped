312722b001d2d9ef92ea0cc1a8e26a6e8292d7d6
Aditya Raj Aryan <adityaraja338@gmail.com>
2025-01-20 09:57:33 +0530

    Dashboard UI updated

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  // baseUrl: 'http://localhost:3000/',
+  baseUrl: 'https://stagingapi.cuetcorner.in/',
+  // baseUrl: 'https://api.cuetcorner.in/',
+  clientId:
+    '834932707480-uvmoe5v9umcbuo7l7jtofe3qg2einp99.apps.googleusercontent.com',
+  redirectUri: 'https://stagingfrontend.cuetcorner.in/login',
+  razorpayKey: 'rzp_test_xtHIdQfnQPITKE',
+};
